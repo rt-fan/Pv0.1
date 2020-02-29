e99951c266003d39fb9abadffe805a8a3b3d817e
Ilyas K <32903511+rt-fan@users.noreply.github.com>
2020-02-29 17:57:41 +0300

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(user="root", password="RT258387684z1", host="127.0.0.1", database="rtdb")
+conn = mysql.connector.connect(user="root", password=" ", host="127.0.0.1", database="rtdb")
 cursor = conn.cursor(buffered=True)
 print('Подключение к БД прошло успешно.')
 
@@ -9,10 +9,10 @@ name = 'London'
 mac = 'cc:ff:ff:ff:ff:ff'
 
 # Запись в базу данных
-# add_unit = ("INSERT INTO unit "
-#             "(unit_ip, unit_name, unit_mac) "
-#             "VALUES ('%(ip)s', '%(name)s', '%(mac)s')" % {'ip': ip, 'name': name, 'mac': mac})
-# cursor.execute(add_unit)
+add_unit = ("INSERT INTO unit "
+            "(unit_ip, unit_name, unit_mac) "
+            "VALUES ('%(ip)s', '%(name)s', '%(mac)s')" % {'ip': ip, 'name': name, 'mac': mac})
+cursor.execute(add_unit)
 
 # Чтение из базы данных
 cursor.execute("SELECT * FROM unit WHERE id = 6")

a2d7aab527ace38bf1127393c22a8b60ae76daae
Ilyas K <32903511+rt-fan@users.noreply.github.com>
2020-03-25 16:42:36 +0300

    Update telnet_connect.py

--- telnet_connect.py
+++ telnet_connect.py
@@ -1,16 +1,29 @@
 import telnetlib
 import time
+import ipaddress
+
 
 host = ''
 login = ''
 password = ''
-command = 'ping ...'
+
+ip2 = host.split('.')
+ip3 = ip2[0] + '.' + ip2[1] + '.' + ip2[2] + '.0/24'
+ip0 = ip2[0] + '.' + ip2[1] + '.' + ip2[2] + '.0'
+subnet1 = ipaddress.ip_network(ip3)
 
 telnet = telnetlib.Telnet(host)
 time.sleep(0.1)
 telnet.write(b'\n')
 telnet.write(login.encode('ascii') + b'\n')
 telnet.write(password.encode('ascii') + b'\n')
-telnet.write(command.encode('ascii') + b'\n')
+
+for ip in subnet1:
+    if ip != ip0:
+        command = 'ping ' + str(ip)
+        telnet.write(command.encode('ascii') + b'\n')
+        print(ip)
+        time.sleep(5)
+
 time.sleep(0.2)
 telnet.close()
